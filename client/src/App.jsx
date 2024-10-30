1ac37a2bd2511207b01fd5e6674005d91a8ba313
lasith herath <lasithherath00@gmail.com>
2024-10-30 07:36:16 +0530

    Set the front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import './App.css';
 function App() {
   const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [country, setCountry] = useState('');
+  const [position, setPosition] = useState('');
+  const [wage, setWage] = useState('');
+
+  const displayInfo = () => {
+    console.log(name + age + country + position + wage);
+  };
   return (
     <>
       <div
@@ -13,21 +21,36 @@ function App() {
         }}
       >
         <div className="information">
-          <label>Name</label>
-          <input type="text" name="" />
-
+          <label>Name:{name}</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <label>Age</label>
-          <input type="number" name="" />
-
+          <input
+            type="number"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
           <label>Country</label>
-          <input type="text" name="" />
-
+          <input
+            type="text"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
           <label>Position</label>
-          <input type="text" name="" />
-
+          <input
+            type="text"
+            value={position}
+            onChange={(e) => setPosition(e.target.value)}
+          />
           <label>Wage (year)</label>
-          <input type="number" name="" />
-
+          <input
+            type="number"
+            value={wage}
+            onChange={(e) => setWage(e.target.value)}
+          />
           <button>Add Employee</button>
         </div>
       </div>
